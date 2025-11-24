54e6d2be9c1e70aa0a6bb54f9b4a436ec052ad42
NataliaYC <natalia.m.yanez.correa@gmail.com>
2025-11-24 12:38:54 -0300

    Finalización y entrega del portafolio módulo 2

--- asset/js/jsmug40.js
+++ asset/js/jsmug40.js
@@ -10,18 +10,18 @@ let itemCount = 0;
 // 1. Definir la función que actualiza el carrito
 function updateCartCounter() {
     // Aumentar el contador en 1
-    itemCount = itemCount + 1;
-
+    itemCount = itemCount + 1; 
+    
     // O de forma abreviada: itemCount++;
 
     // Actualizar el texto del badge en la Navbar
     cartCounter.textContent = itemCount;
-
+    
     // efecto visual al agregar
-    cartCounter.classList.add('animate__pulse');
+    cartCounter.classList.add('animate__pulse'); 
     setTimeout(() => {
         cartCounter.classList.remove('animate__pulse');
-    }, 500);
+    }, 500); 
 }
 
 // 2. Asignar la función a cada botón de producto
@@ -29,9 +29,10 @@ addToCartButtons.forEach(button => {
     button.addEventListener('click', () => {
         // Llamar a la función cada vez que se hace clic
         updateCartCounter();
-
+        
         // Mostrar un mensaje
         const productId = button.getAttribute('data-product-id');
         console.log(`Producto ${productId} agregado al carrito.`);
     });
 });
+
